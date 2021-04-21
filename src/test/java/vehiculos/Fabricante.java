4f2a3a236a9c821e227536ac4c438f610ff4f301
hebermudezg <hebermudezg@unal.edu.co>
2021-04-20 23:47:20 -0500

    ...

--- src/test/java/vehiculos/Fabricante.java
+++ src/test/java/vehiculos/Fabricante.java
@@ -5,7 +5,6 @@ public class Fabricante {
 	Pais pais;
 	
 	public Fabricante(String nombre, Pais pais) {
-		super();
 		this.nombre = nombre;
 		this.pais = pais;
 	}
--- src/test/java/vehiculos/Pais.java
+++ src/test/java/vehiculos/Pais.java
@@ -2,9 +2,9 @@ package vehiculos;
 
 public class Pais {
 	String nombre;
+	int cant = 0;
 
 	public Pais(String nombre) {
-		super();
 		this.nombre = nombre;
 	}
 	
